7de245f3ec33b1381b3dc320adbf6b9f24bb1af8
Donghan Kwak <donghan.kwak@sk.com>
2017-06-22 13:19:03 +0900

    APPEND_SLASH = False 설

--- plusFriendAPI/urls.py
+++ plusFriendAPI/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^keyboard/', include('keyboardAPI.urls')),
-    url(r'^message/', include('contentParser.urls')),
+    url(r'^message', include('contentParser.urls')),
 ]
