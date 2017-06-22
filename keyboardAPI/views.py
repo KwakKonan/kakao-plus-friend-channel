059f9cf4fb041820b3f43d6ca0a36333410ce599
Donghan Kwak <donghan.kwak@sk.com>
2017-06-22 13:01:43 +0900

    플러스친구 API타입 변

--- keyboardAPI/views.py
+++ keyboardAPI/views.py
@@ -5,8 +5,7 @@ import json
 # Create your views here.
 def testResponse(request):
     data = {
-        "type": "buttons",
-        "buttons": ["선택 1", "선택 2", "선택 3"]
+      "type": "text"
     }
     print()
     return HttpResponse(json.dumps(data), status=200)
