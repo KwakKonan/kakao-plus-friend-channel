349214990d130c44d19be065e02512c7c74e317c
Donghan Kwak <donghan.kwak@sk.com>
2017-06-22 12:46:35 +0900

    Keyboard 테스트용 코

--- keyboardAPI/views.py
+++ keyboardAPI/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 # Create your views here.
-def testResponse():
-    return HttpResponse(status=200)
+def testResponse(request):
+    data = {
+        "type": "buttons",
+        "buttons": ["선택 1", "선택 2", "선택 3"]
+    }
+    print()
+    return HttpResponse(json.dumps(data), status=200)
